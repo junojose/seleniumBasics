751cc84a13a444999e240db5d0383cb5efbe132c
junojose <junojose91@gmail.com>
2023-04-03 11:26:34 +0530

    commit all

--- src/test/java/SeleniumBasicCommands/DemoWebShoap.java
+++ src/test/java/SeleniumBasicCommands/DemoWebShoap.java
@@ -26,8 +26,8 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import com.google.common.collect.Table.Cell;
 
 public class DemoWebShoap {
@@ -53,12 +53,14 @@ public class DemoWebShoap {
 		driver.manage().deleteAllCookies();
 	}
 
-	@BeforeMethod
-	public void setUp() {
-		testInitialise("chrome");
+	@BeforeMethod(alwaysRun=true)
+	@Parameters({"browser","base_url"})
+	public void setUp(String browserName,String url) {
+		testInitialise(browserName);
+		driver.get(url);
 	}
 
-	@AfterMethod
+	@AfterMethod(alwaysRun=true)
 	public void tearDown(ITestResult result) throws IOException {
 
 		if (result.getStatus() == ITestResult.FAILURE) {
@@ -66,11 +68,11 @@ public class DemoWebShoap {
 			File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
 			FileUtils.copyFile(screenshot, new File("./Screenshots/" + result.getName() + ".png"));
 		}
-		driver.close();
-		// driver.quit();
+		//driver.close();
+		driver.quit();
 	}
 
-	@Test
+	@Test(priority=1,enabled=true,description="TC001 verify Obsqura Title",groups= {"Regression"})
 	public void TC_001_verifyObsquraTitle() {
 		driver.get("https://selenium.obsqurazone.com/index.php");
 
@@ -80,9 +82,9 @@ public class DemoWebShoap {
 		Assert.assertEquals(actualTitle, expectedTitle, "Invalid Title Found");
 	}
 
-	@Test
+	@Test(priority=2,enabled=true,description="TC002 verify Demo Shoap Login page",groups= {"Regression","Sanity"})
 	public void TC_002_verifyDemoShopLoginPage() {
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		WebElement loginLink = driver.findElement(By.xpath("//a[text()='Log in']"));
 		loginLink.click();
 		WebElement emailField = driver.findElement(By.name("Email"));
@@ -93,9 +95,9 @@ public class DemoWebShoap {
 		loginbutton.click();
 	}
 
-	@Test
+	@Test(priority=3,enabled=true,description="TC003 verify Demo page Registration",groups= {"Smoke"})
 	public void TC_003_verifyDemoPageRegistration() throws InterruptedException {
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		WebElement registerLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
 		registerLink.click();
 		List<WebElement> gender = driver.findElements(By.xpath("//input[@name='Gender']"));
@@ -130,10 +132,10 @@ public class DemoWebShoap {
 
 	}
 
-	@Test
+	@Test(priority=4,enabled=true,description="TC004 verify Title from Excel ",groups= {"Regression"})
 	public void TC_004_verifyTitleFromExcel() throws IOException {
 
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		String actualTitle = driver.getTitle();
 		String excelPath = "\\src\\test\\resources\\TestData.xlsx";
 		String sheetName = "HomePage";
@@ -142,9 +144,9 @@ public class DemoWebShoap {
 
 	}
 
-	@Test
+	@Test(priority=1,enabled=true,description="TC005 verify Registration from Excel sheet",groups= {"Sanity"})
 	public void TC_005_verifyRegistrationFromExcelSheet() throws IOException {
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		WebElement registerLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
 		registerLink.click();
 		String actualTitle = driver.getTitle();
@@ -174,9 +176,9 @@ public class DemoWebShoap {
 
 	}
 
-	@Test
+	@Test(priority=1,enabled=true,description="TC006 verify Demo Web shop Title from excel sheet using List",groups= {"Regression"})
 	public void TC_006_verifyDemoWebShopTitleFromExcelSheetUsingList() throws IOException {
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		String actTitle = driver.getTitle();
 		System.out.println("Actual" + actTitle);
 		List<ArrayList<String>> data = ExcelUtility.excelDataReader("\\src\\test\\resources\\TestData.xlsx",
@@ -186,9 +188,9 @@ public class DemoWebShoap {
 		Assert.assertEquals(actTitle, expTitle, "Invalid Title Found");
 	}
 
-	@Test(dataProvider = "InvalidCredentials")
+	@Test(priority=1,enabled=true,description="TC007 verify login with invalid datas",groups= {"Smoke"},dataProvider = "InvalidCredentials")
 	public void TC_007_verifyLoginWithInvalidDatas(String uname, String password) { /* dataprovider */
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		WebElement loginLink = driver.findElement(By.xpath("//a[text()='Log in']"));
 		loginLink.click();
 		WebElement emailField = driver.findElement(By.xpath("//input[@id='Email']"));
@@ -209,9 +211,9 @@ public class DemoWebShoap {
 				{ "test123@test.com", "123456" } };
 		return data;
 	}
-	@Test
+	@Test(priority=1,enabled=true,description="TC008 verify registration using random generator",groups= {"Regression"})
 	public void TC_008_verifyRegistrationUsingRandomGenerator() throws InterruptedException {
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		WebElement registerLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
 		registerLink.click();
 		String firstName=RandomUtility.getfName();
@@ -238,10 +240,10 @@ public class DemoWebShoap {
 		Assert.assertEquals(actualMail,emailId, "Invalid Mail found");
 
 		}
-	@Test(dataProvider="ValidCredentials")
+	@Test(priority=1,enabled=true,description="TC009 verify login using data provider ",groups= {"Regression"},dataProvider="ValidCredentials")
 	public void TC_009_verifyLoginUsingDataProvider(String uname,String password)
 	{
-		driver.get("https://demowebshop.tricentis.com/");
+		//driver.get("https://demowebshop.tricentis.com/");
 		WebElement loginLink = driver.findElement(By.xpath("//a[text()='Log in']"));
 		loginLink.click();
 		WebElement emailField = driver.findElement(By.xpath("//input[@id='Email']"));
@@ -257,5 +259,52 @@ public class DemoWebShoap {
 		Object[][] data= {{"jun123@gmail.com","test@123"},{"kitjose00@gmail.com","kit@123"}};
 		return data;
 		
+	}
+	@Test(priority=1,enabled=true,description="TC010 login with valid datas using parameters ",groups= {"Regression"})
+	@Parameters({"uName","password"})
+	public void TC_010_verifyLoginWithValidDatasUsingParameters(String uname,String password)
+	{
+		//driver.get("https://demowebshop.tricentis.com/");
+		WebElement loginLink = driver.findElement(By.xpath("//a[text()='Log in']"));
+		loginLink.click();
+		WebElement emailField = driver.findElement(By.xpath("//input[@id='Email']"));
+		WebElement passwordField = driver.findElement(By.xpath("//input[@id='Password']"));
+		emailField.sendKeys(uname);
+		passwordField.sendKeys(password);
+		WebElement loginButton = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+		loginButton.click();
+	}
+	@Test(priority=1,enabled=true,description="TC011 verify registration from excelsheet and mail from random generator",groups= {"Regression"})
+	public void TC_011_verifyRegistrationFromExcelSheetAndMailAsRandomGenerator() throws IOException
+	{
+		//driver.get("https://demowebshop.tricentis.com/");
+		WebElement registerLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
+		registerLink.click(); 
+		List<ArrayList<String>> data=ExcelUtility.excelDataReader("\\src\\test\\resources\\TestData.xlsx","RegisterPage");
+		String expectedTitle = data.get(1).get(0);
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle, "Invalid Registration Title Found");
+		List<WebElement> gender = driver.findElements(By.xpath("//input[@name='Gender']"));
+		selectGender("F", gender);
+		WebElement fName = driver.findElement(By.xpath("//input[@id='FirstName']"));
+		WebElement lName = driver.findElement(By.xpath("//input[@id='LastName']"));
+		WebElement email = driver.findElement(By.xpath("//input[@id='Email']"));
+		WebElement pword = driver.findElement(By.xpath("//input[@id='Password']"));
+		WebElement confirmpword = driver.findElement(By.xpath("//input[@id='ConfirmPassword']"));
+		WebElement registerButton = driver.findElement(By.xpath("//input[@id='register-button']"));
+        String firstName=data.get(1).get(2);//kit
+        String lastName=data.get(1).get(3);//lin
+        String emailId=RandomUtility.getRandomEmail();
+        String Password=data.get(1).get(5);
+        String confirmPassword=data.get(1).get(5);
+        fName.sendKeys(firstName);
+        lName.sendKeys(lastName);
+        email.sendKeys(emailId);
+        pword.sendKeys(Password);
+        confirmpword.sendKeys(confirmPassword);
+        registerButton.click();
+        WebElement validationMail = driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
+		String actualMail = validationMail.getText();
+		Assert.assertEquals(actualMail,emailId, "Invalid Mail found");
 	}
 }
